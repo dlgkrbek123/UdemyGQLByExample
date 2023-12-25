608daf22075a85a09f6c3973d83f2d99fcf598c7
hakyu <dlgkrbek123@gmail.com>
2023-12-25 10:47:27 +0900

    error 학습 완료

--- job-board/client/src/pages/CompanyPage.js
+++ job-board/client/src/pages/CompanyPage.js
@@ -5,11 +5,29 @@ import JobList from '../components/JobList.js';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
 
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
+    // async 즉시 호출
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false });
+      } catch (e) {
+        console.log('error:', JSON.stringify(e, null, 2));
+        setState({ company: null, loading: false, error: true });
+      }
+    })();
   }, []);
+
+  const { company, loading, error } = state;
+
+  if (loading) return <div>Loading....</div>;
+  if (error) return <div className="has-text-danger">Data unavailable</div>;
 
   return (
     company && (
