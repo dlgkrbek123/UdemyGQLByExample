fe1b2c0e0c15a16997785642f34781638c39c530
Hakyu Lee <dlgkrbek123@gmail.com>
2023-12-30 00:05:54 +0900

    Subscription 클라이언트 설정

--- chat/client/src/lib/graphql/client.js
+++ chat/client/src/lib/graphql/client.js
@@ -4,8 +4,13 @@ import {
   concat,
   createHttpLink,
   InMemoryCache,
+  split,
 } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient as createWsClient } from "graphql-ws";
 import { getAccessToken } from "../auth";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { Kind, OperationTypeNode } from "graphql";
 
 const authLink = new ApolloLink((operation, forward) => {
   const accessToken = getAccessToken();
@@ -16,9 +21,28 @@ const authLink = new ApolloLink((operation, forward) => {
   }
   return forward(operation);
 });
-const httpLink = createHttpLink({ uri: "http://localhost:9000/graphql" });
+
+const httpLink = concat(
+  authLink,
+  createHttpLink({ uri: "http://localhost:9000/graphql" })
+);
+
+const isSubscription = (operation) => {
+  const definition = getMainDefinition(operation.query);
+
+  return (
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    definition.operation === OperationTypeNode.SUBSCRIPTION
+  );
+};
+
+const wsLink = new GraphQLWsLink(
+  createWsClient({
+    url: "ws://localhost:9000/graphql",
+  })
+);
 
 export const apolloClient = new ApolloClient({
-  link: concat(authLink, httpLink),
+  link: split(isSubscription, wsLink, httpLink),
   cache: new InMemoryCache(),
 });
